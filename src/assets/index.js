eb6e8deb7776d00d909e24fab2cf8793da66b9e6
Anthony Dourado <135552243+Code-With-Anthony@users.noreply.github.com>
2025-01-03 16:31:33 +0530

    sd
    sd

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+import captureIcon from "./captureIcon.png";
+import flashOff from "./flashOff.png";
+import flashOn from "./flashOn.png";
+import uploadIcon from "./uploadIcon.png";
+
+export {
+    captureIcon,
+    flashOff,
+    flashOn,
+    uploadIcon
+}
